631ab7a149a653e0cf5618934563c086ecd7697d
Jasur Mirboboyev <mirboboyevjasurjan@gmail.com>
2025-09-04 02:10:36 +0500

    env

--- server-local.js
+++ server-local.js
@@ -5,7 +5,7 @@ const { bot, ensureBotInit } = require("./src/bot");
 (async () => {
   try {
     await ensureBotInit();
-    console.log("✅ Bot init — long polling rejimida ishlayapti (local test)");
+    console.log("✅ Bot init — long polling rejimida ishlayapti (local test) ");
     await bot.start();
   } catch (e) {
     console.error("❌ Xato: ", e);
